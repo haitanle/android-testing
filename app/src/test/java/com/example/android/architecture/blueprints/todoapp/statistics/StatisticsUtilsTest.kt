052ee6d1565e7499499ea6018e0ef7045c7dcb4a
Le, Hai Tan <Hai.Le@duke-energy.com>
2020-05-21 15:09:38 -0400

    test-two

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -0,0 +1,24 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.junit.Assert.*
+import org.junit.Test
+
+/**
+ * Created by tanle on 5/21/20.
+ */
+
+class StatisticsUtilsTest{
+
+    @Test
+    fun getActiveAndCompletedStats_noCompleted_returnsHundredZero() {
+        val tasks = listOf<Task>(
+                Task("title", "desc", isCompleted = false)
+        )
+
+        val result = getActiveAndCompletedStats(tasks)
+
+        assertEquals(result.completedTasksPercent, 0f)
+        assertEquals(result.activeTasksPercent, 100f)
+    }
+}
