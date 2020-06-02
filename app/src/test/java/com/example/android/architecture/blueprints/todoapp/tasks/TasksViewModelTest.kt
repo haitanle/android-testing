b96b6e3199a05d09984fa1604638ea59639cc2f9
Le, Hai Tan <Hai.Le@duke-energy.com>
2020-06-02 14:40:52 -0400

    test-five 10.Task: Writing multiple ViewModel tests

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -0,0 +1,38 @@
+package com.example.android.architecture.blueprints.todoapp.tasks
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.Event
+import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Created by tanle on 6/2/20.
+ */
+
+@RunWith(AndroidJUnit4::class)
+class TasksViewModelTest {
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Test
+    fun addNewTasks_callTaskEvent(){
+        val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+
+        val observer = Observer<Event<Unit>> {}
+
+        tasksViewModel.newTaskEvent.observeForever(observer)
+
+        tasksViewModel.addNewTask()
+
+        val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
+        assertThat(value.getContentIfNotHandled(), (not(nullValue())))
+    }
+}
