f79a140a4883884769696957193561504329fa4d
Le, Hai Tan <Hai.Le@duke-energy.com>
2020-06-29 11:47:12 -0400

    9. Task: Make a ServiceLocator

---
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
@@ -0,0 +1,28 @@
+package com.example.android.architecture.blueprints.todoapp.taskdetail
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import com.example.android.architecture.blueprints.todoapp.R
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Created by tanle on 6/26/20.
+ */
+
+@MediumTest
+@RunWith(AndroidJUnit4::class)
+class TaskDetailFragmentTest {
+
+    @Test
+    fun activeTaskDetails_DisplayedInUi(){
+        val activeTask = Task("Active Task", "AndroidX Rocks", false)
+
+        val bundle = TaskDetailFragmentArgs(activeTask.id).toBundle()
+        launchFragmentInContainer<TaskDetailFragment>(bundle, R.style.AppTheme)
+        Thread.sleep(2000)
+    }
+}
